60006e5ae02cc86bf291615e92ffecc64bbf16e8
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-20 17:02:27 +0300

    added test for flat-json

---
+++ _tests_/index.test.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'node:fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '_fixtures_', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('check flat json', () => {
+  const expectedResult = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const result = readFile('result-json.txt');
+  expect(expectedResult).toBe(result);
+});

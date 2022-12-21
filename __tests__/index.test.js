8d30878476765ea6b0e7700d292cf7171c23db6e
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-21 16:52:53 +0300

    updated functions

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,6 +10,18 @@ const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test('check flat json', () => {
   const expectedResult = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const result = readFile('result-json.txt');
+  const result = readFile('result-stylish.txt');
+  expect(expectedResult).toBe(result);
+});
+
+test('check flat yml', () => {
+  const expectedResult = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+  const result = readFile('result-stylish.txt');
+  expect(expectedResult).toBe(result);
+});
+
+test('check flat yaml', () => {
+  const expectedResult = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'));
+  const result = readFile('result-stylish.txt');
   expect(expectedResult).toBe(result);
 });
--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
 import { readFileSync } from 'node:fs';
-import _ from 'lodash';
+import path from 'path';
+import getParsedData from './parsers.js';
+import getDiff from './diffGenerator.js';
 
-const readFile = (filepath) => readFileSync(filepath, 'utf8');
+const getData = (filepath) => readFileSync(filepath, 'utf8');
+const getFormat = (filepath) => path.extname(filepath).slice(1);
 
-const parseFile = (data) => JSON.parse(data);
+export default (filepath1, filepath2) => {
+  const format1 = getFormat(filepath1);
+  const format2 = getFormat(filepath2);
 
-const getDiff = (obj1, obj2) => {
-  const keys1 = _.keys(obj1);
-  const keys2 = _.keys(obj2);
-  const sortedKeys = _.sortBy(_.union(keys1, keys2));
+  const data1 = getParsedData(getData(filepath1), format1);
+  const data2 = getParsedData(getData(filepath2), format2);
 
-  const diff = sortedKeys.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-
-    if (!_.has(obj1, key)) {
-      return `  + ${key}: ${value2}`;
-    }
-    if (!_.has(obj2, key)) {
-      return `  - ${key}: ${value1}`;
-    }
-    if (value1 === value2) {
-      return `    ${key}: ${value2}`;
-    }
-    return `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
-  });
-  const result = diff.join('\n');
-  return `{\n${result}\n}`;
-};
-
-const genDiff = (filepath1, filepath2) => {
-  const data1 = parseFile(readFile(filepath1));
-  const data2 = parseFile(readFile(filepath2));
   const result = getDiff(data1, data2);
   return result;
 };
-
-export default genDiff;

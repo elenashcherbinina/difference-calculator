1dc972de14da7a06943140bcd056f365b55dbd1f
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-21 15:11:38 +0300

    new name for directories fixtures and tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,7 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '_fixtures_', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test('check flat json', () => {

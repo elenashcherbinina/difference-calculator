79b588f16349199a0d0b46fe2f7152ea6783ba17
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-29 11:24:36 +0300

    added function for stylish-format

---
+++ src/formatters/stylish.js
@@ -0,0 +1,54 @@
+import _ from 'lodash';
+
+const replacer = ' ';
+const standartSpacesCount = 2;
+const spacesCount = 4;
+
+const getIndent = (depth = 1) => replacer.repeat(depth * spacesCount - standartSpacesCount);
+
+const stringify = (value, depth = 1) => {
+  const currentIndent = getIndent(depth + 1);
+  const bracketIndent = currentIndent.slice(2);
+
+  if (!_.isObject(value)) return value;
+
+  const lines = Object.entries(value).map(([key, val]) => `${currentIndent}  ${key}: ${stringify(val, depth + 1)}`);
+  return ['{', ...lines, `${bracketIndent}}`].join('\n');
+};
+
+const getTree = (node, depth = 1) => {
+  const currentIndent = getIndent(depth);
+  const bracketIndent = currentIndent.slice(2);
+
+  const result = node.map(({ key, type, value, children, valueFrom, valueTo }) => {
+    const data = `${key}: ${stringify(value, depth)}`;
+
+    switch (type) {
+      case 'added': {
+        return `${currentIndent}+ ${data}`;
+      }
+      case 'deleted': {
+        return `${currentIndent}- ${data}`;
+      }
+      case 'unchanged': {
+        return `${currentIndent}  ${data}`;
+      }
+      case 'changed': {
+        return [
+          `${currentIndent}- ${key}: ${stringify(valueFrom, depth)}`,
+          `${currentIndent}+ ${key}: ${stringify(valueTo, depth)}`,
+        ].join('\n');
+      }
+      case 'nested': {
+        return `${currentIndent}  ${key}: ${getTree(children, depth + 1)}`;
+      }
+      default:
+        throw new Error(`Type ${type} is not defined`);
+    }
+  });
+  return ['{', ...result, `${bracketIndent}}`].join('\n');
+};
+
+const stylish = (tree) => getTree(tree, 1);
+
+export default stylish;

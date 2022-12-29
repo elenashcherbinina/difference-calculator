bc31b86753d2d2514b36941817b46e52c9d143d3
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-29 11:25:57 +0300

    added stylish as default format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version')
   .helpOption('-h, --HELP', 'output usage information')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));

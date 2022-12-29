ef4220cb0186c5125fc2ac5ea0434b07f455bbe2
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-29 11:22:33 +0300

    added tests and files for nested-trees

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,23 +5,19 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const getData = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('check flat json', () => {
-  const expectedResult = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const result = readFile('result-stylish.txt');
-  expect(expectedResult).toBe(result);
-});
-
-test('check flat yml', () => {
-  const expectedResult = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
-  const result = readFile('result-stylish.txt');
-  expect(expectedResult).toBe(result);
-});
-
-test('check flat yaml', () => {
-  const expectedResult = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'));
-  const result = readFile('result-stylish.txt');
+test.each([
+  ['file1.json', 'file2.json', 'result-stylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'result-stylish.txt'],
+  ['file1.yml', 'file2.yml', 'result-stylish.txt'],
+  ['file3.json', 'file4.json', 'result-4-5.txt'],
+  ['file3.yaml', 'file4.yaml', 'result-4-5.txt'],
+  ['file3.yml', 'file4.yml', 'result-4-5.txt'],
+])('check diff', (file1, file2, resultFile) => {
+  const expectedResult = genDiff(getFixturePath(file1), getFixturePath(file2));
+  const result = getData(resultFile);
   expect(expectedResult).toBe(result);
 });

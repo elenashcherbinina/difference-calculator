1c2fa6d4835a7a7e98be0486f71dba18a7c60034
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-29 11:25:12 +0300

    new name and refactoring

---
+++ src/buildAST.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+
+const buildAST = (obj1, obj2) => {
+  const keys1 = _.keys(obj1);
+  const keys2 = _.keys(obj2);
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
+
+  const result = sortedKeys.map((key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { key, type: 'nested', children: buildAST(value1, value2) };
+    }
+    if (!_.has(obj1, key)) {
+      return { key, type: 'added', value: value2 };
+    }
+    if (!_.has(obj2, key)) {
+      return { key, type: 'deleted', value: value1 };
+    }
+    if (value1 === value2) {
+      return { key, type: 'unchanged', value: value2 };
+    }
+    return {
+      key,
+      type: 'changed',
+      valueFrom: value1,
+      valueTo: value2,
+    };
+  });
+
+  return result;
+};
+
+export default buildAST;

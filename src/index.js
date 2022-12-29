50ed67a170be5d77fa6a7c9ed7b4a5e52b2eca1c
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-29 11:27:36 +0300

    added stylish as default format and import new function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import { readFileSync } from 'node:fs';
 import path from 'path';
 import getParsedData from './parsers.js';
-import getDiff from './diffGenerator.js';
+import buildAST from './buildAST.js';
+import buildStylishTree from './formatters/stylish.js';
 
 const getData = (filepath) => readFileSync(filepath, 'utf8');
 const getFormat = (filepath) => path.extname(filepath).slice(1);
 
-export default (filepath1, filepath2) => {
+export default (filepath1, filepath2, format = 'stylish') => {
   const format1 = getFormat(filepath1);
   const format2 = getFormat(filepath2);
 
   const data1 = getParsedData(getData(filepath1), format1);
   const data2 = getParsedData(getData(filepath2), format2);
 
-  const result = getDiff(data1, data2);
-  return result;
+  const result = buildAST(data1, data2);
+  return buildStylishTree(result, format);
 };

d41b9f367cd1700dba3bdf37601d5d4c5672f226
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-30 11:39:28 +0300

    added format option and new names

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import { readFileSync } from 'node:fs';
 import path from 'path';
 import getParsedData from './parsers.js';
 import buildAST from './buildAST.js';
-import buildStylishTree from './formatters/stylish.js';
+import format from './formatters/index.js';
 
 const getData = (filepath) => readFileSync(filepath, 'utf8');
 const getFormat = (filepath) => path.extname(filepath).slice(1);
 
-export default (filepath1, filepath2, format = 'stylish') => {
+export default (filepath1, filepath2, formatName = 'stylish') => {
   const format1 = getFormat(filepath1);
   const format2 = getFormat(filepath2);
 
   const data1 = getParsedData(getData(filepath1), format1);
   const data2 = getParsedData(getData(filepath2), format2);
 
-  const result = buildAST(data1, data2);
-  return buildStylishTree(result, format);
+  const data = buildAST(data1, data2);
+  return format(data, formatName);
 };

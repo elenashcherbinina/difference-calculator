03aae3ceaf33db94824a00c43877f3981e0e9df8
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-12-30 11:38:37 +0300

    added format-option

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,14 +10,14 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const getData = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each([
-  ['file1.json', 'file2.json', 'result-stylish.txt'],
-  ['file1.yaml', 'file2.yaml', 'result-stylish.txt'],
-  ['file1.yml', 'file2.yml', 'result-stylish.txt'],
-  ['file1.json', 'file2.json', 'result-plain.txt'],
-  ['file1.yaml', 'file2.yaml', 'result-plain.txt'],
-  ['file1.yml', 'file2.yml', 'result-plain.txt'],
-])('check stylish and plain format', (file1, file2, resultFile) => {
-  const expectedResult = genDiff(getFixturePath(file1), getFixturePath(file2));
+  ['file1.json', 'file2.json', 'stylish', 'result-stylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'stylish', 'result-stylish.txt'],
+  ['file1.yml', 'file2.yml', 'stylish', 'result-stylish.txt'],
+  ['file1.json', 'file2.json', 'plain', 'result-plain.txt'],
+  ['file1.yaml', 'file2.yaml', 'plain', 'result-plain.txt'],
+  ['file1.yml', 'file2.yml', 'plain', 'result-plain.txt'],
+])('check diff', (file1, file2, formatName, resultFile) => {
+  const expectedResult = genDiff(getFixturePath(file1), getFixturePath(file2), formatName);
   const result = getData(resultFile);
   expect(expectedResult).toBe(result);
 });

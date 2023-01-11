a967fdb13f43771449fda191724bf09489bf9059
Elena Shcherbinina <el.shcherbinina@outlook.com>
2023-01-11 11:03:11 +0300

    removed old name for argument

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import getParsedData from './parsers.js';
 import buildAST from './buildAST.js';
 import format from './formatters/index.js';
 
-const getFullPath = (filename) => path.resolve(process.cwd(), filename);
-const getData = (filename) => readFileSync(getFullPath(filename), 'utf8');
-const getFormat = (filename) => path.extname(filename).slice(1);
+const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const getData = (filepath) => readFileSync(getFullPath(filepath), 'utf8');
+const getFormat = (filepath) => path.extname(filepath).slice(1);
 
-export default (filename1, filename2, formatName = 'stylish') => {
-  const format1 = getFormat(filename1);
-  const format2 = getFormat(filename2);
+export default (filepath1, filepath2, formatName = 'stylish') => {
+  const format1 = getFormat(filepath1);
+  const format2 = getFormat(filepath2);
 
-  const data1 = getParsedData(getData(filename1), format1);
-  const data2 = getParsedData(getData(filename2), format2);
+  const data1 = getParsedData(getData(filepath1), format1);
+  const data2 = getParsedData(getData(filepath2), format2);
 
   const data = buildAST(data1, data2);
   return format(data, formatName);
